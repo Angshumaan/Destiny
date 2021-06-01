4d63a73bdd5697d0ae6ada4b6c5f978b467461e0
= <=>
2021-06-01 20:24:49 +0530

    deploy

--- trio/settings.py
+++ trio/settings.py
@@ -140,14 +140,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
-
-
 STATICFILES_DIRS = [
     'trio/static',
 ]
 
-STATIC_ROOT = '/var/www/angshu/static'
+STATIC_ROOT = '/var/www/Angshu/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
