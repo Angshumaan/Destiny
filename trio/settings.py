2d82a943d2724854a18f2ff8e3918a478c2e3703
= <=>
2021-06-02 10:34:24 +0530

    initial commit

--- trio/settings.py
+++ trio/settings.py
@@ -30,12 +30,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'some_random_default_string')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['angshu.pythonanywhere.com']
 
 
 # Application definition
