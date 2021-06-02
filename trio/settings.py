b61e01b98ec8d22972e23bdf66d11b38db94b2b4
= <=>
2021-06-02 10:05:59 +0530

    initial commit

--- trio/settings.py
+++ trio/settings.py
@@ -143,7 +143,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    'trio/static',
+    os.path.join(BASE_DIR, "trio/static")
+
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
